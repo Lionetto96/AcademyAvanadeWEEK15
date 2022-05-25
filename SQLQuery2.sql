b1332a0f899629cf753fba103ec6fc28d222a960
ALE <93939843+Lionetto96@users.noreply.github.com>
2022-05-25 18:43:46 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+insert into Employees 
+values (1,'Martina','Tiso'),
+(2,'Alessia','Lionetto'),
+(3,'Maria','De Stefano')
+select * from Employees
+
+insert into Projects 
+values (1,'Project1','Cliente1'),(2,'Project2','Cliente2'),(3,'Project3','Cliente3')
+select * from Projects
+
+insert into Timesheets 
+values (2,30,'2021-02-11 00:00:00',1,2),(3,45,'2021-02-02 00:00:00',2,3)
+select * from Timesheets
